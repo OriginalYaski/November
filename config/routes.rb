97255a5edffa59cfe3ba275ad73e63cf90c78fbb
OriginalYaski <113488867+OriginalYaski@users.noreply.github.com>
2022-12-11 17:42:57 -0700

    Attempting to set up portfolio page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: "home#index"
   resources :projects
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  get '/projects/portfolio', to: 'projects#portfolio'
 
   # Defines the root path route ("/")
   # root "articles#index"
