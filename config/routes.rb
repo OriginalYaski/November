3882cdddd65176baead1008b771f6de7600b2956
OriginalYaski <113488867+OriginalYaski@users.noreply.github.com>
2022-12-11 17:44:50 -0700

    Switched ordering of lines

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
+  get '/projects/portfolio', to: 'projects#portfolio'
   resources :projects
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
-  get '/projects/portfolio', to: 'projects#portfolio'
 
   # Defines the root path route ("/")
   # root "articles#index"
